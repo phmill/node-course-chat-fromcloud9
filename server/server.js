176226511ba9a50049a0d6efbb50ace39f88690b
EC2 Default User <ec2-user@ip-172-31-19-199.us-east-2.compute.internal>
2018-08-18 12:19:43 +0000

    Add geolocation support via geoloction api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require("socket.io");
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath=path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -23,12 +23,15 @@ io.on('connection', (socket) => {
    
     socket.on('createMessage', (newMessage, callback) => {
         console.log('createMessage', newMessage);
-
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
         callback('This is from the server.');
-
         // socket.broadcast.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
     });
+    
+    socket.on('createLocationMessage', (coords) => {
+        console.log(coords);
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    })
    
     socket.on('disconnect', () => {
         console.log('User disconnected');
